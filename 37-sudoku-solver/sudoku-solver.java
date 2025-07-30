0ff7c5ddb9a3f77740842f7e988ab71fd2759455
Piyush Aanand <136635069+piyushaanand@users.noreply.github.com>
2025-07-30 22:49:33 +0530

    Time: 150 ms (90.29%) | Memory: 41.8 MB (8.37%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
+
+    public boolean helper(char[][] board, int row, int col) {
+        if (row == board.length) {
+            return true;
+        }
+        int nrow, ncol;
+        if (col != board.length - 1) {
+            nrow = row;
+            ncol = col + 1;
+        } else {
+            nrow = row + 1;
+            ncol = 0;
+        }
+
+        if (board[row][col] != '.') {
+            return helper(board, nrow, ncol);
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (helper(board, nrow, ncol)) {
+                        return true;
+                    } else {
+                        board[row][col] = '.';
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isSafe(char[][] board, int row, int col, int num) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == (char) (num + '0')) {
+                return false;
+            }
+            if (board[i][col] == (char) (num + '0')) {
+                return false;
+            }
+        }
+        //Grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == (char) (num + '0')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
