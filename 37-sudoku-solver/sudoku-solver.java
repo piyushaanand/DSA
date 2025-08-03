7bd5523eff18adf57eaa30a09426c7719723ec16
Piyush Aanand <136635069+piyushaanand@users.noreply.github.com>
2025-08-03 16:52:56 +0530

    Time: 163 ms (85.29%) | Memory: 41.7 MB (11.65%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -2,28 +2,28 @@ class Solution {
     public void solveSudoku(char[][] board) {
         helper(board, 0, 0);
     }
-
-    public boolean helper(char[][] board, int row, int col) {
-        if (row == board.length) {
+    public boolean helper(char [][]board, int row, int col){
+        int n = board.length;
+        if(row == n){
             return true;
         }
         int nrow, ncol;
-        if (col != board.length - 1) {
+        if(col != n-1){
             nrow = row;
-            ncol = col + 1;
-        } else {
-            nrow = row + 1;
+            ncol = col+1;
+        }else{
+            nrow = row+1;
             ncol = 0;
         }
-        if (board[row][col] != '.') {
+        if(board[row][col] != '.'){
             return helper(board, nrow, ncol);
-        } else {
-            for (int i = 1; i <= 9; i++) {
-                if (isSafe(board, row, col, i)) {
-                    board[row][col] = (char) (i + '0');
-                    if (helper(board, nrow, ncol)) {
+        }else{
+            for(int i = 1 ; i <= 9 ; i++){
+                if(isSafe(board, row, col, i)){
+                    board[row][col] = (char)(i+'0');
+                    if(helper(board, nrow, ncol)){
                         return true;
-                    } else {
+                    }else{
                         board[row][col] = '.';
                     }
                 }
@@ -31,18 +31,17 @@ class Solution {
         }
         return false;
     }
-
-    public boolean isSafe(char[][] board, int row, int col, int num) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == (char) (num + '0') || board[i][col] == (char) (num + '0')) {
+    public boolean isSafe(char[][] board, int row, int col, int num){
+        for(int i = 0 ; i < board.length ; i++){
+            if(board[i][col] == (char)(num+'0') || board[row][i] == (char)(num+'0')){
                 return false;
             }
         }
-        int sr = (row / 3) * 3;
-        int sc = (col / 3) * 3;
-        for (int i = sr; i < sr + 3; i++) {
-            for (int j = sc; j < sc + 3; j++) {
-                if (board[i][j] == (char) (num + '0')) {
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+        for(int i = sr ; i < sr+3; i++){
+            for(int j = sc ; j < sc+3 ; j++){
+                if(board[i][j] == (char)(num+'0')){
                     return false;
                 }
             }
